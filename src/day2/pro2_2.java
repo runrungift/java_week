cebade2c3d45083b03afe98356e7571d08ce96fd
runrungift <nmitsuki.g@gmail.com>
2025-03-19 16:51:13 +0900

    day2pro

---
+++ src/day2/pro2_2.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class pro2_2 {
+    public static void main(String[] args) {
+        int a = 1,b = 2,c = 3,d = 4,e = 5;
+//        a = a + 2;
+//        b = b - 1;
+//        c = c * 3;
+//        d = d / 2;
+//        e = e % 2;
+        a+=2;
+        b-=1;
+        c*=3;
+        d/=2;;
+        e%=2;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+    }
+}
