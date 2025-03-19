8ea9b981327488da4e5a5e959874c2809dee3b77
runrungift <nmitsuki.g@gmail.com>
2025-03-19 22:25:03 +0900

    day3file

---
+++ src/day3/file3.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class file3 {
+    public static void main(String[] args) {
+        int num = 2;
+        if(num == 1){
+            System.out.println("one");
+        }else if(num == 2){
+            System.out.println("two");
+        }else if(num == 3){
+            System.out.println("three");
+        }else{
+            System.out.println("不適切な値です。");
+        }
+    }
+}
---
+++ src/day3/file5.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class file5 {
+    public static void main(String[] args) {
+        int num = 1;
+        switch (num){
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("不適切な値です。");
+        }
+    }
+}
