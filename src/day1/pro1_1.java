07a4bccc59c4ebbb514d0493d733aeba1473d415
runrungift <nmitsuki.g@gmail.com>
2025-03-19 15:46:23 +0900

    pro1_2

--- src/day1/pro1_1.java
+++ src/day1/pro1_1.java
@@ -2,6 +2,6 @@ package day1;
 
 public class pro1_1 {
     public static void main(String[] args) {
-        System.out.println("Lightit");
+        System.out.println("Light");
     }
 }
---
+++ src/day1/pro1_2.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class pro1_2 {
+    public static void main(String[] args) {
+//        System.out.print("ABC");
+//        System.out.print("DEF");
+//        System.out.println("GHI");
+        System.out.println("ABCDEFGHI");
+    }
+}
