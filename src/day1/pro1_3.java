00f3fd59bc69f054eb03027789a01a0e361356ed
runrungift <nmitsuki.g@gmail.com>
2025-03-19 15:48:21 +0900

    pro1_3

---
+++ src/day1/pro1_3.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class pro1_3 {
+    public static void main(String[] args) {
+        System.out.println(1 + "+" + 1 + "=" + (1+1));
+        System.out.println(2 + "+" + 3 + "=" + (2+3));
+    }
+}
